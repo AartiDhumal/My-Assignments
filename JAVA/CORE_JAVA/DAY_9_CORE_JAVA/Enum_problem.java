c289f7f4126890fc27cd1fcbb2753897525c3b51
AartiDhumal <97721849+AartiDhumal@users.noreply.github.com>
2022-08-03 13:07:37 +0530

    Check the output
    Red means Stop
    Green means Go
    Yellow means Drive very slow.
    
    I want the same output

--- JAVA/CORE_JAVA/DAY_9_CORE_JAVA/Enum_problem.java
+++ JAVA/CORE_JAVA/DAY_9_CORE_JAVA/Enum_problem.java
@@ -32,7 +32,7 @@ public class Enum_problem {
 //THE OUTPUT IS:
 /*
 C:\Users\coditas\.jdks\openjdk-18.0.1.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.3\lib\idea_rt.jar=52950:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.3\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\coditas\IdeaProjects\CORE JAVA_DAY 2\out\production\CORE JAVA_DAY 2" DAY_9_CORE_JAVA.Enum_problem
-Stop
+Red means Stop
 GO
 Drive very slow
 
